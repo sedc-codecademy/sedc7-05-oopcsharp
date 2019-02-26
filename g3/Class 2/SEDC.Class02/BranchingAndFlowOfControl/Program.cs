c1c7cad383a60cd9dd7e2b71bde74179c38461f8
dragan <dragang@iborn.net>
2019-02-26 21:18:20 +0100

    Class 2 code

---
+++ g3/Class 2/SEDC.Class02/BranchingAndFlowOfControl/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAndFlowOfControl
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region if/else
+			if (5 > 3)
+			{
+				Console.WriteLine("5 is larger than 3");
+			}
+			else
+			{
+				Console.WriteLine("5 is not larger than 3"); // Visual studio tells us here that this code will never be executed
+			}
+
+			Console.Write("Please enter a number: ");
+			int someNumber = int.Parse(Console.ReadLine());
+			if (someNumber > 0)
+			{
+				Console.Write("Number is positive");
+			}
+			else if (someNumber < 0)
+			{
+				Console.Write("Number is negative");
+			}
+			else
+			{
+				Console.Write("Number is zero");
+			}
+			#endregion
+			#region switch
+			int day = 3;
+			switch (day)
+			{
+			case 1: // We can test multiple cases at a time if they have the same outcome
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+				Console.WriteLine("Work day :(");
+				break;
+			case 6:
+			case 7:
+				Console.WriteLine("WEEKEND! WOHO!");
+				break;
+			default:
+				Console.WriteLine("There is no such day! Sorry!"); // This happens if none of the above passes
+				break;
+			}
+			#endregion
+			#region 5 - 7
+			// Exercise 5
+			Console.Write("Please enter number of trees: ");
+			int trees = int.Parse(Console.ReadLine());
+			int n = 8;
+			int m = 5;
+			int treeApples = n * 12 * trees;
+			int result;
+			if (treeApples % m == 0)
+			{
+				result = treeApples / m;
+			}
+			else
+			{
+				result = treeApples / m + 1;
+			}
+			Console.WriteLine("You would need :" + result + " baskets!");
+			Console.ReadLine();
+
+			// Exercise 6
+			Console.Write("Enter a number: ");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.Write("Enter another number: ");
+			int num2 = int.Parse(Console.ReadLine());
+			int larger;
+			if (num1 > num2)
+			{
+				larger = num1;
+				Console.WriteLine("First is larger!");
+			}
+			else if (num2 > num1)
+			{
+				larger = num2;
+				Console.WriteLine("Second is larger!");
+			}
+			else
+			{
+				larger = num1;
+				Console.WriteLine("The numbers are equal!");
+			}
+
+			if (larger != 0)
+			{
+				if (larger % 2 == 0)
+				{
+					Console.WriteLine("Number is Even!");
+				}
+				else if (larger % 2 != 0)
+				{
+					Console.WriteLine("Number is Odd!");
+				}
+			}
+			else
+			{
+				Console.WriteLine("You entered Zero. That is not even nore odd!");
+			}
+
+			// Exercise 7
+			Console.WriteLine("Whats behind door 1,2,3");
+			int door;
+			bool isNumber = int.TryParse(Console.ReadLine(), out door);
+			if (isNumber)
+			{
+				switch (door)
+				{
+				case 1:
+					Console.WriteLine("You got a new car");
+					break;
+				case 2:
+					Console.WriteLine("You got a new plane");
+					break;
+				case 3:
+					Console.WriteLine("You got a new house");
+					break;
+				default:
+					Console.WriteLine("That was not 1 2 or 3! Sorry!");
+					break;
+				}
+			}
+			else
+			{
+				Console.WriteLine("That was not a number! Sorry!");
+			}
+			#endregion
+		}
+	}
+}
---
+++ g3/Class 2/SEDC.Class02/DataTypes/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Data Types
+			int integerVariable1 = 5;
+			int integerVariable2;
+			integerVariable2 = -100;
+			integerVariable2 = 12;
+			float floatVariable = 2.5F;
+			double doubleVariable = 2.5;
+			string stringVariable = "Hello SEDC!";
+			char characterVariable = 'd';
+			bool booleanVariable = false;
+			long longerInteger = 3242343243243242342;
+			int sumOfTwoNumbers = 2 + 6;
+			sumOfTwoNumbers += 12;
+			//sumOfTwoNumbers++;
+			bool isLarger = 6 > 12;
+			Console.WriteLine(isLarger);
+			bool someExpression = true;
+			someExpression &= isLarger;
+			var x = 5;
+			//x = "Bobi";
+			Console.WriteLine(someExpression);
+			//Console.WriteLine(sumOfTwoNumbers++);
+			//Console.WriteLine(sumOfTwoNumbers++);
+			//Console.WriteLine(sumOfTwoNumbers);
+			//Console.WriteLine(integerVariable1);
+			//Console.WriteLine(integerVariable2);
+			Console.ReadLine();
+			#endregion
+			#region Variables
+
+			// Operations in variables
+			int sumOfTwoNumbers = 5 + 2;
+			int someNumber1 = 5;
+			int someNumber2 = 2;
+			int SumOfTwoVariables = someNumber1 + someNumber2;
+			// = means take the value of
+			someNumber2 = someNumber1;
+			// You can't take the value from another variable if its not the same type
+			string someString1 = "this is another string";
+			// If you uncomment the line bellow it will give you an error
+			//someString1 = someNumber1;
+			#endregion
+			#region Exercises 1 - 4
+			// Exercise 1
+			int a1; // declaration
+			a1 = 4; // initialization
+			int b1;
+			b1 = 12;
+			int result1 = a1 / b1;
+			Console.WriteLine("Exercise 1 int Result:" + result1);
+
+			double a2 = 4;
+			double b2 = 12;
+			double result2 = a2 / b2;
+			Console.WriteLine("Exercise 1 double Result:" + result2);
+			Console.ReadLine();
+
+			// Exercise 2
+			string a3 = "Dejan";
+			string b3 = "Jovanov";
+			string result3 = a3 + " " + b3; // Adding empty space to divide the strings
+			Console.WriteLine("Exercise 2 strings Result:" + result3);
+
+			string a4 = "5";
+			string b4 = "7";
+			string result4 = a4 + b4;
+			Console.WriteLine("Exercise 2 numbers Result:" + result4);
+			Console.ReadLine();
+
+			// Exercise 3
+			int a5 = 9;
+			string b5 = "Code";
+			string result5 = a5 + b5;
+			Console.WriteLine("Exercise 3 Result:" + result5);
+			Console.ReadLine();
+
+			// Exercise 4
+			int credit = 102;
+			int messageCost = 5;
+			int messagesSent = credit / messageCost;
+			Console.WriteLine("You can send: " + messagesSent + " messages!");
+			Console.ReadLine();
+			#endregion
+		}
+	}
+}
