30b6e71667ce95ed403dc3d61848e5385a9f2133
Igor Mitkovski <igor.mitkovski@outlook.com>
2019-02-27 21:09:33 +0100

    add class 2

---
+++ g2/Class 2/Class2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string num = "9 " + " " + "5";
+            int branches = 12;
+            int applesOnBranch = 8;
+            int basketSize = 5;
+            Console.Write("Vnesete broj na drva: ");
+            string trees = Console.ReadLine();
+            double numTrees = double.Parse(trees);
+            double dblBranches = Convert.ToDouble(branches);
+            double dblApplesPerBranch = Convert.ToDouble(applesOnBranch);
+            double dblBasketSize = Convert.ToDouble(basketSize);
+            var neededBaskets = numTrees * dblBranches * dblApplesPerBranch / dblBasketSize;
+            var roundedBaskets = Math.Ceiling(neededBaskets);
+            Console.WriteLine(roundedBaskets);
+
+            Console.ReadLine();
+        }
+    }
+}
