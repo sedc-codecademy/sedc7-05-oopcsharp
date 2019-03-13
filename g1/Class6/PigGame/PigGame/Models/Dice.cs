7b3fbe3b5e5793e45d176df5bedc6e7e8f671cbe
unknown <you@example.com>
2019-03-13 20:42:09 +0100

    Added class 6 exercise

---
+++ g1/Class6/PigGame/PigGame/Models/Dice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigGame.Models
+{
+    class Dice
+    {
+        public enum Colors {White, Red, Blue };
+        public enum Sizes {Low, Medium, High};
+        public Dice(Random rnd)
+        {
+            this.rnd = rnd;
+            this.Value = 0;
+            this.Color = Colors.White;
+            this.Size = Sizes.Medium;
+        }
+        private Random rnd;
+        public Colors Color { get; set; }
+        public Sizes Size { get; set; }
+        public int Value { get; set; }
+        public void Roll()
+        {
+            this.Clear();
+            this.Value = rnd.Next(1, 7);
+        }
+        private void Clear()
+        {
+            this.Value = 0;
+        }
+    }
+}
