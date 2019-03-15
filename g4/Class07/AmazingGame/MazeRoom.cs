a994454ff2c58fcdc5130878d2ece50bc52f8312
Wekoslav Stefanovski <swekster@gmail.com>
2019-03-15 19:14:42 +0100

    Added amazing example

---
+++ g4/Class07/AmazingGame/MazeRoom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazingGame
+{
+    public class MazeRoom
+    {
+        public string Name { get; set; }
+
+        public bool HasNorthDoor { get; set; }
+        public bool HasSouthDoor { get; set; }
+        public bool HasWestDoor { get; set; }
+        public bool HasEastDoor { get; set; }
+
+        public string CheckDoor(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.North:
+                    break;
+                case Direction.South:
+                    break;
+                case Direction.East:
+                    break;
+                case Direction.West:
+                    break;
+                default:
+                    throw new Exception("Something broke");
+            }
+            return "NE RABOTE";
+        }
+
+        public void CheckDoors()
+        {
+            CheckDoor(Direction.North);
+            CheckDoor(Direction.South);
+            CheckDoor(Direction.East);
+            CheckDoor(Direction.West);
+        }
+    }
+}
---
+++ g4/Class07/AmazingGame/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maze maze = new Maze();
+
+            MazeRoom room = new MazeRoom
+            {
+                Name = "The Room",
+                HasNorthDoor = true,
+                HasEastDoor = true
+            };
+
+            room.CheckDoors();
+
+        }
+    }
+}
