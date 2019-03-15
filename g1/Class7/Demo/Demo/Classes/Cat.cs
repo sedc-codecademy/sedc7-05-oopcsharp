8903ef4c447b6ef9954eb1d0e1409975ba6267c6
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2019-03-15 08:18:45 +0100

    Added class7

---
+++ g1/Class7/Demo/Demo/Classes/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Cat:Animal
+    {
+        public Cat()
+        {
+            Console.WriteLine("Cat constructor");
+        }
+        ~Cat()
+        {
+            Console.WriteLine("Cat destructor");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g1/Class7/Exercise/Inheritance/Classes/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Classes
+{
+    // Animal class, a parent class from which other classes will inherit
+    public class Animal
+    {
+        // These properties will be shared with all classes that inherit from this one
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        // Eet method that all classes that inherit from this one can call
+        // We write virtual if we want this method to be overriden by child classes
+        public virtual void Eat()
+        {
+            Console.WriteLine($"The {Type} animal named {Name} is eating!");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Id: {Id}, Animal: {Type}, Name: {Name}");
+        }
+
+        // Default constructor of animal. It will be called when new instance of Animal is created
+        // It will also be called when new instance of any other child class is created
+        // The parent constructor always executes first
+        public Animal()
+        {
+            Console.WriteLine("A new instance of an animal has been created. This is the Animal constructor.");
+        }
+
+        // Another constructor that takes a string as parameter
+        public Animal(string type)
+        {
+            Type = type;
+            Console.WriteLine("This is an ID constructor of Animal.");
+        }
+    }
+}
