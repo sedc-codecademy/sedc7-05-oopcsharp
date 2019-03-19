fc0d815554a3cee4a3e8500dc59a10220d7da343
dragan <dragang@iborn.net>
2019-03-19 21:04:24 +0100

    Class 9 code

---
+++ g3/Class 9/SEDC.Class09/Exercises/Entities/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Rating { get; set; }
+        public double TicketPrice { get; set; }
+
+        public Movie(string title, Genre genre, int rating, double ticketPrice)
+        {
+            Title = title;
+            Genre = genre;
+            try
+            {
+                if (rating > 0 && rating < 6)
+                {
+                    Rating = rating;
+                }
+                else
+                {
+                    throw new Exception("Rating must be from 1 to 5");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            TicketPrice = Rating * ticketPrice;
+        }
+    }
+}
