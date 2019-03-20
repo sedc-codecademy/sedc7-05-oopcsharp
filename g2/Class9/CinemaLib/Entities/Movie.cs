d40d2d50909c13c68e45462d2de9047d9d900890
Igor Mitkovski <igor.mitkovski@outlook.com>
2019-03-20 20:40:29 +0100

    add code from class 9

---
+++ g2/Class9/CinemaLib/Entities/Movie.cs
@@ -0,0 +1,37 @@
+﻿using CinemaLib.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaLib.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Rating { get; set; }
+        public double TicketPrice { get; set; }
+
+        public Movie(string title, Genre genre, int rating)
+        {
+            Title = title;
+            Genre = genre;
+            try
+            {
+                if(rating < 0 || rating > 5)
+                {
+                    throw new Exception("Rating must be between 1 and 5");
+                }
+                Rating = rating;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            TicketPrice = 5 * Rating;
+        }
+    }
+}
---
+++ g2/Class9/Exceptions/Program.cs
@@ -0,0 +1,61 @@
+﻿using CinemaLib.Entities;
+using CinemaLib.Generator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region examples
+            //try
+            //{
+            //    int result = DivideTwoNums();
+            //    Console.WriteLine(result);
+            //}
+            //catch (FormatException e)
+            //{
+
+            //    Console.WriteLine("Format Exception: " +
+            //        "You entered a character" +
+            //        " which is not a number");
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    Console.WriteLine("Divide by zero ex: " +
+            //        "You tried divison with zero");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            #endregion
+
+            List<Movie> movieList = GenerateMovies.GenerateFirstList();
+
+            Cinema Cineplexx = new Cinema("Cineplexx", new List<int> { 1, 2, 3, 4 }, movieList);
+
+            Console.WriteLine("Got to end");
+            Console.ReadLine();
+        }
+
+        public static int DivideTwoNums()
+        {
+            Console.Write("Enter a number: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            int second = int.Parse(Console.ReadLine());
+            if (first < 0)
+            {
+                new Exception();
+                throw new Exception("You entered a negative number");
+            }
+            return first / second;
+        }
+    }
+}
