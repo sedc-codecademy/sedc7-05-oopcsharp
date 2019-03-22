7c6e750a4cf487412cc646258c938f7168d9e3f5
Wekoslav Stefanovski <swekster@gmail.com>
2019-03-22 21:12:21 +0100

    Added exception examples

---
+++ g4/Class09/OurOwnException/Movie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurOwnException
+{
+    class Movie
+    {
+        public int Rating { get; private set; }
+        public string Name { get; private set; }
+
+        public Movie (string name)
+        {
+            Name = name;
+        }
+
+        public void SetRating(int rating)
+        {
+            if (rating > 0 && rating <=10)
+            {
+                Rating = rating;
+            }
+            else
+            {
+                throw new MovieException("Rating is out of range");
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Rating != 0)
+            {
+                return $"The movie {Name} has a rating of {Rating} stars";
+            }
+            else
+            {
+                return $"The movie {Name} is unrated";
+            }
+        }
+
+
+    }
+}
---
+++ g4/Class09/OurOwnException/MovieException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurOwnException
+{
+    class MovieException: ApplicationException
+    {
+
+        public MovieException() : base()
+        {
+
+        }
+
+
+        public MovieException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
